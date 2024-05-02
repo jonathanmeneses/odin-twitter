3155b0ff9eba026d39deab72b363c87f37439e34
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-05-01 23:23:22 -0500

    Add username and name fields to Devise

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "feeds#show"
+
+  get 'feed', to: 'feeds#show'
+
 end
