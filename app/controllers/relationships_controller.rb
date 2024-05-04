d18af526623fdc89bd7c8a73fb7f1f86e8737390
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-05-04 00:20:50 -0500

    Added custom precompiling. Kind of a de-rail

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,15 @@
+class RelationshipsController < ApplicationController
+  before_action authenticate_user!
+
+  def create
+    @user = User.find(params[:followed_id])
+    current_user.following << @user
+  end
+
+  def destroy
+    @relationship = current_user.active_relationships.find(params[:id])
+    @user = @relationship.followed
+    current_user.following.delete(@user)
+  end
+
+end
